fd888ce6d58dfe8e4ba647557129e8df38c7daa3
mahesh <mail.mahesh.lk@gmail.com>
2021-01-05 12:32:51 +0800

    Go operands & cons

--- gooperands.go
+++ gooperands.go
@@ -7,14 +7,20 @@ func main() {
   var lvalu = "abc"
   var rvalu = "Abc"
   var rs = true
+  var decvalu string = "abc"
+  
 
   if(lvalu==rvalu) {
      rs = true
-} else {
+  } else {
     rs = false
 
-}
+  }
   fmt.Println(rs)
+
+  if(lvalu==decvalu) {
+   fmt.Println(" no diff dec or default type ")
+  }
 
 }
 
