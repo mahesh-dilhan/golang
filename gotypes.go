16d584b932faafd91ed440956e301ff87d403b31
mahesh <mail.mahesh.lk@gmail.com>
2021-01-05 10:49:40 +0800

    Go variable

---
+++ gotypes.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var fnumber float64
+  fnumber = 100.0
+  fmt.Println(fnumber)
+  fmt.Printf("type %T \n", fnumber)
+  inumber := 10
+  fmt.Println(inumber)
+
+}
