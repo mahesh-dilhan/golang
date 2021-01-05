0d0f81df8e2664e138a6ae9a31e6f473f5e4e01c
mahesh <mail.mahesh.lk@gmail.com>
2021-01-05 16:05:35 +0800

    Go condition

---
+++ gocondition.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+ typeOfMe := func(t interface{}) {
+  switch i := t.(type) {
+   case bool:
+    fmt.Println("this is boolean")
+   default: 
+    fmt.Printf("no idea %T\n", i)
+  }
+
+}
+
+ typeOfMe(true)
+}
