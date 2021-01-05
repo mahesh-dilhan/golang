2aaa69b20b3df1f72dee15484037e61e586fc933
mahesh <mail.mahesh.lk@gmail.com>
2021-01-05 11:55:41 +0800

    Go operands & cons

---
+++ gocons.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+
+ const EXRATE float64 = 142.45
+ var transferamt float64 = 100
+
+ fmt.Println(EXRATE * transferamt)
+  
+
+}
---
+++ gooperands.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+  var lvalu = "abc"
+  var rvalu = "Abc"
+  var rs = true
+
+  if(lvalu==rvalu) {
+     rs = true
+} else {
+    rs = false
+
+}
+  fmt.Println(rs)
+
+}
+
