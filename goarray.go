0b7fd338c47826a766fb4974da562bd892a321b3
mahesh <mail.mahesh.lk@gmail.com>
2021-08-11 16:06:26 +0800

    go-slice

--- goarray.go
+++ goarray.go
@@ -4,14 +4,21 @@ import "fmt"
 
 func main() {
 
- arr := [5]int{1,2,3,4,5}
+	arr := [5]int{1, 2, 3, 4, 5}
 
- fmt.Println("list", arr)
+	fmt.Println("list", arr)
 
-arr[2] = 10
-fmt.Println("list", arr)
+	arr[2] = 10
+	fmt.Println("list", arr)
 
- var a int = 10
- fmt.Println(a) 
+	var a int = 10
+	fmt.Println(a)
 
+	for _, value := range arr {
+		fmt.Println(value)
+	}
+
+	var sl []int = arr[1:2]
+
+	fmt.Println(sl)
 }
