44cc91da6463bf02188d1e46c8166a95654083d0
mahesh <mail.mahesh.lk@gmail.com>
2021-08-12 21:11:02 +0800

    spread...unpack

--- goslice.go
+++ goslice.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	s := [...]int{1, 2, 3, 4, 5}
+	s := []int{1, 2, 3, 4, 5}
 	for a, b := range s {
 		fmt.Println(a, b)
 	}
+
+	s2 := []int{10, 11}
+	s = append(s, s2...)
+
+	fmt.Println(s)
+
 }
