3280dca9dcc569439bc4c7f4a9aa7975614f20d1
mahesh <mail.mahesh.lk@gmail.com>
2021-10-02 19:18:16 +0800

    promoted

--- goloop.go
+++ goloop.go
@@ -4,12 +4,20 @@ import "fmt"
 
 func main() {
 
-   i := 1
-   for i <= 10 {
-     fmt.Printf("this is %d " ,i)
-     i++
-  
-  }
+	i := 1
+	for i <= 10 {
+		fmt.Printf("this is %d ", i)
+		i++
+	}
 
+	fmt.Println("")
+	sum := 0
+	for i := 1; i < 5; i++ {
+		if i%2 != 0 { // skip odd numbers
+			continue
+		}
+		sum += i
+	}
+	fmt.Println(sum) // 6 (2+4)
 
 }
