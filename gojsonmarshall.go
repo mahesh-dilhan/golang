d9346c728ae45d295ba998182d228669fa30205b
mahesh <mail.mahesh.lk@gmail.com>
2021-10-04 22:18:03 +0800

    promoted

--- gojsonmarshall.go
+++ gojsonmarshall.go
@@ -10,10 +10,21 @@ type Person struct {
 	Age  int    `json:"age"`
 }
 
+type Bird struct {
+	Species     string
+	Description string
+}
+
 func main() {
 
 	p := Person{"Alice", 22}
 
 	jsonData, _ := json.Marshal(p)
 	fmt.Println(string(jsonData))
+
+	birdJson := `{"species": "pigeon","description": "likes to perch on rocks"}`
+	var bird Bird
+	json.Unmarshal([]byte(birdJson), &bird)
+	fmt.Printf("Species: %s, Description: %s", bird.Species, bird.Description)
+
 }
